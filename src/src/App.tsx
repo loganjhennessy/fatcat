8d4e7bca05ae06014c6df93237c660d3f938f139
Logan Hennessy <loganjhennessy@gmail.com>
2020-03-28 23:02:03 -0700

    Initial react commit

---
+++ src/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import StockTable from './StockTable';
+
+function App() {
+  return (
+    <div className="App">
+      <StockTable/>
+    </div>
+  );
+}
+
+export default App;
